84ce05e55fa69fa6d917b2ba61c0160cbcf0509b
Casey Johnson <johnsoncm3@gmail.com>
2021-06-23 00:48:53 -0400

    deploying

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.use(express.static('public'));
 mongoose.connect('mongodb://localhost/workout' , { 
      useNewUrlParser: true,
      useFindAndModify: false,
-     useUnifiedTopology: true
+     useUnifiedTopology: true,
+     useCreateIndex: true
     });
 
 
